604f4e607346c184a4038424e155aaf74342eb2f
Andi Alifsyah <andyalyfsyah4@gmail.com>
2021-07-03 22:17:32 +0800

    relation penginapan lokasi

--- app/Models/lokasi.php
+++ app/Models/lokasi.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class lokasi extends Model
 {
     use HasFactory;
     public $timestamps = false;
+    protected $primaryKey = 'id_lokasi';
 
+    /**
+     * Get all of the penginapan for the lokasi
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function penginapan(): HasMany
+    {
+        return $this->hasMany(Penginapan::class, 'id_lokasi', 'id_lokasi');
+    }
 }
