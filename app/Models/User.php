e7cdc1a903e01214ae0cc2c80a2d7527f03e96da
Andi Alifsyah <andyalyfsyah4@gmail.com>
2021-07-04 20:26:38 +0800

    username as primary key

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
      */
     protected $guarded = [];
     public $timestamps = false;
+    protected $primaryKey = 'username';
 
     /**
      * The attributes that should be hidden for arrays.
